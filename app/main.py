f53185a92221fc0dbf952fec7af21ad3d8509b76
ProviDER... <128478616+uyg7x@users.noreply.github.com>
2026-03-16 16:40:12 +0530

    Delete app directory

--- app/main.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-from app.ui.dashboard import PigmentProcessMonitoringApp
-
-
-def launch() -> None:
-    root = tk.Tk()
-    PigmentProcessMonitoringApp(root)
-    root.mainloop()
